0d21f266fab76d42e3e4ac0d71fc4b0cab1dea4f
khaifazil <khairal.fazil@gmail.com>
2022-04-01 04:09:53 +0800

    completed activity

--- temperatureSensor.go
+++ temperatureSensor.go
@@ -1,6 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 func main() {
-	//temperature := [24]float64{20.1, 24, 27.3, 30.1, 26.4, 22.2, 20.1, 24, 27.3, 30.1, 26.4, 20.1, 24, 27.3, 30.1, 26.4, 20.1, 24, 27.3, 30.1, 26.4, 20.1, 24, 27.3}
+	temperature := [24]float64{20.1, 24, 27.3, 30.1, 26.4, 22.2, 20.1, 24, 27.3, 30.1, 26.4, 20.1, 24, 27.3, 30.1, 26.4, 20.1, 24, 27.3, 30.1, 26.4, 20.1, 24, 27.3}
 	//Insert Code here
+	var total float64
+
+	for _, temp := range temperature {
+		total += temp
+	}
+	// avgTemp := total/24
+	fmt.Println(math.Round(total/24*10)/10)
 }
